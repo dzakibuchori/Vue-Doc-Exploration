96f9775b1f4db658c79d4513d4327745e8d4b286
dzakibuchori <dzaki.buchori@yahoo.com>
2020-07-25 13:14:39 +0700

    on click

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11'
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue.js!'
   }
 });
 
@@ -32,5 +32,24 @@ var app4 = new Vue({
       { text: 'Learn Vue' },
       { text: 'Build something awesome' }
     ]
+  }
+});
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: "Hello Vue.js!"
+  },
+  methods: {
+    reverseMessage: function() {
+      this.message = this.message.split('').reverse().join('');
+    }
+  }
+});
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: "Hello Vue.js!"
   }
 });
